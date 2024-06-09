21842d446d8b61288b84d13238c91b3f2c81eb8f
NishantGada <nishant.gada160@gmail.com>
2024-06-09 11:55:02 +0530

    Inital Commit for App.js file

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './pages/Home';
+import ProfileScreen from './pages/Profile';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar, StyleSheet, View } from 'react-native';
+import Home from './pages/Home';
+import Appointments from './pages/Appointments';
+import Appbar from './components/Appbar';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+/*
+
+USE STACK NAVIGATION WHEN USING HEIRARCHICAL STRUCTURE. SUCH AS NAVIGATING WITHIN DIFFERENT SECTIONS.
+
+function HomeStack() {
+	return (
+		<Stack.Navigator>
+			<Stack.Screen name="Home" component={HomeScreen} />
+		</Stack.Navigator>
+	);
 }
 
+function ProfileStack() {
+	return (
+		<Stack.Navigator>
+			<Stack.Screen name="Profile" component={ProfileScreen} />
+		</Stack.Navigator>
+	);
+}
+*/
+
+function App() {
+	return (
+		<SafeAreaView style={styles.safeArea}>
+			<Appbar />
+			<NavigationContainer>
+				<StatusBar style="auto" />
+				<Tab.Navigator>
+					<Tab.Screen
+						options={{
+							headerShown: false
+						}}
+						name="Home"
+						component={HomeScreen} />
+					<Tab.Screen
+						options={{
+							headerShown: false
+						}}
+						name="Appointments"
+						component={Appointments} />
+					<Tab.Screen
+						options={{
+							headerShown: false
+						}}
+						name="Profile"
+						component={ProfileScreen} />
+				</Tab.Navigator>
+			</NavigationContainer>
+		</SafeAreaView>
+	);
+}
+
+export default App;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	safeArea: {
+		flex: 1,
+		// borderWidth: 3,
+        // borderColor: 'brown',
+		padding: 10
+	},
+})
