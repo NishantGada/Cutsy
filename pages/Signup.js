4840f6cf7cee438e40a4c60b5259e0da94bfb3b1
NishantGada <nishant.gada160@gmail.com>
2024-06-19 14:13:13 +0530

    Code clean up on Signup Page

--- pages/Signup.js
+++ pages/Signup.js
@@ -1,10 +1,25 @@
 import { View, Text, Button, StyleSheet } from 'react-native'
 import React from 'react'
 import UserSignupForm from '../components/forms/userSignupForm'
+import sampleUsers from '../utils/sampleUsers.json';
+import sendRequest from '../utils/sendRequest';
 
 export default function Signup({ navigation }) {
 
-    const handleOnSignup = () => {navigation.navigate('Login')}
+    const handleOnSignup = async () => {
+        try {
+            var response = await sendRequest(
+                "http://192.168.29.188:8080/create-user",
+                sampleUsers[1],                
+                "POST",
+                {}
+            )
+
+            navigation.navigate('Login')
+        } catch (error) {
+            console.log("error: " + error);
+        }
+    }
 
     return (
         <View style={styles.container}>
