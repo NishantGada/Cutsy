b91ba17c3a23167ff7b6455c07407f814b441d5a
NishantGada <nishant.gada160@gmail.com>
2024-06-20 17:29:10 +0530

    Added modal to confirm successful sign up

--- pages/Signup.js
+++ pages/Signup.js
@@ -1,38 +1,67 @@
-import { View, Text, Button, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, Button, StyleSheet, Dimensions, Modal } from 'react-native'
+import React, { useState } from 'react'
 import UserSignupForm from '../components/forms/userSignupForm'
 import sampleUsers from '../utils/sampleUsers.json';
 import sendRequest from '../utils/sendRequest';
 
-export default function Signup({ navigation }) {
+const screenWidth = Dimensions.get('window').width;
 
-    const handleOnSignup = async () => {
+export default function Signup({ navigation }) {
+    const [showModal, setShowModal] = useState(false);
+
+    const openModal = () => {
+        setShowModal(true)
+    }
+    const closeModal = () => {
+        setShowModal(false)
+    }
+
+    const callCreateUserAPI = async () => {
         try {
             var response = await sendRequest(
                 "http://192.168.29.188:8080/create-user",
-                sampleUsers[1],                
+                sampleUsers[1],
                 "POST",
                 {}
             )
 
+            console.log("callCreateUserAPI response: ", response);
             navigation.navigate('Login')
         } catch (error) {
-            console.log("error: " + error);
+            console.log("callCreateUserAPI error: " + error);
         }
+    }
+
+    const handleOnSignup = () => {
+        console.log("Inside handleOnSignup");
+        openModal()
+        setTimeout(() => {
+            closeModal()
+            callCreateUserAPI()
+        }, 1000 * 2);
+    }
+
+    const handleExistingUser = () => {
+        navigation.navigate('Login')
     }
 
     return (
         <View style={styles.container}>
+            <View style={styles.modalContainer}>
+                <Modal
+                    visible={showModal}
+                    transparent={true}
+                    animationType="slide"
+                >
+                    <View style={styles.modalOverlay}>
+                        <View style={styles.newAppointmentModal}>
+                            <Text>Successfully Logged in!</Text>
+                        </View>
+                    </View>
+                </Modal>
+            </View>
             <Text>Signup Screen</Text>
-            {/* <Button
-                title="Go to Home"
-                onPress={() => navigation.navigate('Main')}
-            /> */}
-            {/* <Button
-                title="Go to Login"
-                onPress={() => navigation.navigate('Login')}
-            /> */}
-            <UserSignupForm handleOnSignup={handleOnSignup} />
+            <UserSignupForm handleOnSignup={handleOnSignup} handleExistingUser={handleExistingUser} />
         </View>
     )
 }
@@ -40,6 +69,28 @@ export default function Signup({ navigation }) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingVertical: 20
+        paddingVertical: 20,
+        backgroundColor: "white",
+        rowGap: 10
     },
+    modalContainer: {
+        // flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    modalOverlay: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    newAppointmentModal: {
+        backgroundColor: 'white',
+        width: screenWidth * 0.9,
+        height: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        padding: 20,
+    }
 })
